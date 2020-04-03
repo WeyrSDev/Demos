276f3e196b2a0471f10b060c467e0b901810b2e7
Damian Yarker <damian_yarker@hotmail.com>
2020-04-03 18:31:02 +0100

    update

--- Playground/ch3 Work 2.cpp
+++ Playground/ch3 Work 2.cpp
@@ -12,6 +12,69 @@
 //////////////////////////////////////////////////
 using namespace std;
 
-int main() {
+char ToUpperCase(char c);
+char ToLowerCase(char c);
 
+int main() {
+	char input = 0;
+
+	for (int i = 97; i <= 122; i++) {
+		if (i % 5 == 0) {
+			cout << endl;
+			cout << ToUpperCase(i) << ", ";
+		}
+	}
+
+	VISPACE;
+
+	for (int i = 65; i <= 90; i++) {
+		if (i % 5 == 0) {
+			cout << endl;
+			cout << ToLowerCase(i) << ", ";
+		}
+	}
+
+	cout << "Enter a lower case char: ";
+	cin >> input;
+
+	VISPACE;
+
+	cout << "Your char = " << ToUpperCase(input) << endl;
+
+	VISPACE;
+
+	cout << "Enter upper case char: ";
+	cin >> input;
+
+	VISPACE;
+
+	cout << "Your char = " << ToLowerCase(input) << endl;
+}
+
+char ToUpperCase(char c) {
+	if (c < 65 || (c > 91 && c < 96) || c > 122) {
+		return '\0';
+	}
+	else {
+		if (c - 32 < 65) {
+			return c;
+		}
+		else {
+			return (c - 32);
+		}
+	}
+}
+
+char ToLowerCase(char c) {
+	if (c < 65 || (c > 91 && c < 97) || c > 122) {
+		return '\0';
+	}
+	else {
+		if (c + 32 > 122) {
+			return c;
+		}
+		else {
+			return (c + 32);
+		}
+	}
 }
