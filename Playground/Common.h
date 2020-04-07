2fc64e8bd17c7fcf97205856b9bbc7cfd81980b2
Damian Yarker <damian_yarker@hotmail.com>
2020-04-07 05:10:23 +0100

    Functions

--- Playground/Common.h
+++ Playground/Common.h
@@ -21,7 +21,37 @@
 #include <stdlib.h>
 //////////////////////////////////////////////////
 #define VISPACE cout << endl << endl;
-typedef unsigned char viChar;
+
+typedef char                    viChar;
+typedef unsigned char           viByte;
+typedef char                    viInt8;
+typedef unsigned char           viUInt8;
+typedef short                   viInt16;
+typedef unsigned short          viUInt16;
+typedef long                    viInt32;
+typedef unsigned long           viUInt32;
+typedef signed __int64          viInt64;
+typedef unsigned __int64        viUInt64;
+typedef int                     viInt;      // Platform specific integer type (i.e. 32 vs 64 bit).
+typedef unsigned int            viUInt;     // Platform specific unsigned integer type (i.e. 32 vs 64 bit).
+typedef float                   viFloat;    // Explicit single precision.
+typedef double                  viDouble;   // Explicit double precision.
+typedef float                   viReal;     // Configurable single / double precision.
+
+
+const auto VI_PI = (viReal)3.141592654;
+const auto VI_QUARTER_PI = (viReal)VI_PI / 4.0f;
+const auto VI_HALF_PI = (viReal)VI_PI / 2.0f;
+const auto VI_DOUBLE_PI = (viReal)VI_PI * 2.0F;
+const auto VI_EPSILON_1M = (viReal)1.0;            // 1 meter (m) tolerance (1)
+const auto VI_EPSILON_1CM = (viReal)0.01;          // 1 centimeter (cm) tolerance (1e-2)
+const auto VI_EPSILON_1MM = (viReal)0.001;         // 1 millimeter (mm) tolerance (1e-3)
+const auto VI_EPSILON_1UM = (viReal)0.000001;      // 1 micrometer (um) tolerance (1e-6)
+const auto VI_EPSILON_1NM = (viReal)0.000000001;   // 1 nanometer (nm) tolerance (1e-9) 
+const auto VI_EPSILON_SINGLE = FLT_EPSILON;        // Single precision epsilon
+const auto VI_EPSILON_DOUBLE = DBL_EPSILON;        // Double precision epsilon
+const auto VI_EPSILON = 1e-5f;                     // Generic 0-1 range epsilon (useful for normal tests, divide by 0 tests, etc.)
+
 /*
 	Fibonacci: 1, 1, 2, 3, 5, 8, 13, 21
 	Lucas: 1, 3, 4, 7, 11, 18, 29, 47
---
+++ Playground/extra functions 1.cpp
@@ -0,0 +1,62 @@
+//////////////////////////////////////////////////
+//
+//           
+//           
+//           Copyright (c) 2020 WeyrSoftware
+//
+//////////////////////////////////////////////////
+//
+//           Includes
+//////////////////////////////////////////////////
+#include "Common.h"
+//////////////////////////////////////////////////
+using namespace std;
+
+// Numeric
+
+// Basic Math
+
+
+// Exponential Functions
+
+
+// Logarithmic Functions
+
+
+// Power Functions
+
+
+// Trigonometric Functions
+
+
+// Hyperbolic Functions
+
+
+// Integral Rounding
+
+
+
+// Function Declarations
+viFloat viArea(viFloat p_radius);	
+viInt viCube(viInt p_number);
+viInt viAverage(viInt p_number);
+viInt viFactorial(viInt p_number);
+viFloat viRandomFloat(viFloat p_range1, viFloat p_range2);
+viInt viRandomInt(viInt p_range1, viInt p_range2);
+viInt viToRadian();
+viInt viToDegree();
+viInt viCompare();
+viChar viToLowerCase(viChar p_char);
+viChar viToUpperCase(viChar p_char);
+viFloat viDistance3D(viFloat p_ux, viFloat p_uy, viFloat p_uz, viFloat p_vx, viFloat p_vy, viFloat p_vz);
+
+
+
+
+
+// Application Main
+int main() {
+	cout << "WTF" << endl;
+
+
+}
